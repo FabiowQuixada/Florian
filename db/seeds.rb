a50aec7a7eb811cbc7aac34052b82754244739ed
Fabiow Servidor IAQ <ftquixada@gmail.com>
2016-01-26 21:40:03 -0300

    Dados Reais no Seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
 
 role1 = Role.create({name: "Admin", description: 'Administrador'})
 role2 = Role.create({name: "Usuário", description: 'Usuário comum'})
-role3 = Role.create({name: "Secretária 1", description: '(Renata)'})
-role4 = Role.create({name: "Secretária 2", description: '(Lucivania)'})
+role3 = Role.create({name: "Administração", description: 'Presidência'})
+role4 = Role.create({name: "Secretária 1", description: '(Renata)'})
+role5 = Role.create({name: "Secretária 2", description: '(Lucivania)'})
 
-user1 = User.create({name: "Sistema",     email: "apoioaoqueimado@yahoo.com.br", password: "fulano0123", password_confirmation: "fulano0123", bcc: 'teste@test.com', signature: 'teste', role: role1})
-user2 = User.create({name: "Fabiow",      email: "ftquixada@gmail.com", password: "fulano0123", password_confirmation: "fulano0123", bcc: 'teste@test.com', signature: 'teste', role: role1})
-user3 = User.create({name: "Edmar",       email: "edmar@gmail.com", password: "fulano0123", password_confirmation: "fulano0123", bcc: 'teste@test.com', signature: 'teste', role: role1})
-user4 = User.create({name: "Márcia",       email: "marcia@gmail.com", password: "fulano0123", password_confirmation: "fulano0123", bcc: 'teste@test.com', signature: 'teste', role: role1})
-user5 = User.create({name: "Lucivânia",   email: "luci@yahoo.com.br", password: "senha_renata", password_confirmation: "senha_renata", bcc: 'teste@test.com', signature: 'teste', role: role4})
-user6 = User.create({name: "Renata",       email: "fquixada@yahoo.com.br", password: "senha_renata", password_confirmation: "senha_renata", bcc: 'teste@test.com', signature: 'teste', role: role3})
-user7 = User.create({name: "Inativo",       email: "teste_inativo@yahoo.com.br", password: "usuario_teste", password_confirmation: "usuario_teste", bcc: 'teste@test.com', signature: 'teste', role: role2, active: false})
+user1 = User.create({name: "Sistema",     email: "apoioaoqueimado@yahoo.com.br", password: "fulano0123", password_confirmation: "fulano0123", bcc: 'teste@test.com', signature: '(Assinatura)', role: role1})
+user2 = User.create({name: "Fabiow",      email: "ftquixada@gmail.com", password: "fulano0123", password_confirmation: "fulano0123", bcc: 'teste@test.com', signature: '(Assinatura)', role: role1})
+user3 = User.create({name: "Inativo",       email: "teste_inativo@yahoo.com.br", password: "usuario_teste", password_confirmation: "usuario_teste", bcc: 'teste@test.com', signature: '(Assinatura)', role: role2, active: false})
+user4 = User.create({name: "Edmar",       email: "edmarmaciel@gmail.com", password: "35348", password_confirmation: "35348", bcc: 'teste@test.com', signature: '(Assinatura)', role: role3})
+user5 = User.create({name: "Márcia",       email: "marciattm@hotmail.com", password: "34533", password_confirmation: "34533", bcc: 'teste@test.com', signature: '(Assinatura)', role: role3})
+user6 = User.create({name: "Renata",       email: "renata.sbq@gmail.com", password: "46486", password_confirmation: "46486", bcc: 'teste@test.com', signature: '(Assinatura)', role: role4})
+user7 = User.create({name: "Lucivânia",   email: "lucivania_nojosa@hotmail.com", password: "23425", password_confirmation: "23425", bcc: 'teste@test.com', signature: '(Assinatura)', role: role5})
+
 
 comp1 = Company.create(trading_name: "Empresa I", name: "Empresa I Ltda.", cnpj: "31162488000187", category: 1, address: "lala", group: 1)
 comp2 = Company.create(trading_name: "Empresa II", name: "Empresa II Ltda.", cnpj: "34.781.467/0001-38", category: 2, address: "lala",  group: 2)
