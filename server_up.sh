56e44b70ea723ecb8146e75a2d7222a7ce7be8ef
Fabiow Servidor IAQ <ftquixada@gmail.com>
2016-01-31 22:35:15 -0300

    Teste de E-mail de Recibo

--- server_up.sh
+++ server_up.sh
@@ -1,3 +1,3 @@
 RAILS_ENV=production bin/rake assets:precompile
-wheneverize .
+cd $(dirname $0) && bundle exec whenever -s "environment=production" --update && wheneverize .
 cd $(dirname $0) && rails s -e production
