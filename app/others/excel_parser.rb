eef10770c44fd8220a3abb8a22d69e327ec10428
Fabiow <fquixada@yahoo.com.br>
2016-03-05 11:47:14 -0300

    Ajustes no Parser de Excel

--- app/others/excel_parser.rb
+++ app/others/excel_parser.rb
@@ -1,41 +1,55 @@
 class ExcelParser
  def self.parse
 
-         file = Roo::Excel.new(Rails.root.join('app', 'assets', 'test.xls'))
+  inconsistent_companies = Array.new
+
+         file = Roo::Excel.new(Rails.root.join('app', 'assets', 'empresas_2016.xls'))
 
          file.worksheets.each_with_index do |sheet, index|
 
           # Get companies
-          for r in 5..(sheet.count-1)
+          for r in 4..(sheet.count-1)
 
               row = sheet.rows[r]
-
-              #byebug if index > 0
-
-              #byebug if row.nil?
 
               company = parse_company row, (index+1)
 
               if !company.save
-                  puts company.errors
-                  #byebug if index > 0
+                  inconsistent_companies << company
               end
           end # Row loop end
 
          end # Sheet loop end
+
+
+         inconsistent_companies.each do |company|
+          puts company
+
+          company.errors.full_messages.each do |error_message| 
+           puts error_message
+          end 
+
+         end
     end
 
     private
 
     def self.parse_company(row, group)
 
-      col = 1
+      col = 0
       company = Company.new
 
-      company.trading_name = row[col]
-      company.name = row[col]
+      if row[col] == 'PF'
+        company.entity_type = 2
+      else
+        company.entity_type = 1
+      end
 
-      cat = 1#0 ##########################################################
+      col += 1
+      company.registration_name = row[col]
+      company.name = row[col += 1]
+
+      cat = 0
       col += 1
       if row[col] == 'I'
         cat = 1
@@ -45,10 +59,16 @@ class ExcelParser
         cat = 3
       end
 
+      
       company.category = cat
-      company.group = group
+      company.group = 1
 
-      company.cnpj = row[col += 1]
+      if company.person?
+        company.cpf = row[col += 1]
+      else
+        company.cnpj = row[col += 1]
+      end
+
       company.address = row[col += 1]
       company.neighborhood = row[col += 1]
       company.cep = row[col += 1]
@@ -57,8 +77,9 @@ class ExcelParser
       company.email_address = row[col += 1]
       company.website = row[col += 1]
 
+
       # Contacts
-      col = 11
+      col = 12
 
       contact1 = Contact.new
 
@@ -98,6 +119,54 @@ class ExcelParser
       company.contacts.push contact1
       company.contacts.push contact2
       company.contacts.push contact3
+
+      # Donation
+      donation = Donation.new
+      temp = row[col += 1]
+
+      if temp.is_a? Numeric
+        donation.value = temp
+      else
+        donation.remark = temp
+      end
+
+      temp = row[col += 1]
+      if temp == "Mensal"
+        company.payment_frequency = 3
+      elsif temp == "Diariamente"
+        company.payment_frequency = 1
+      elsif temp == "Bimestral"
+        company.payment_frequency = 4
+      elsif temp == "Semanal"
+        company.payment_frequency = 2
+      elsif temp == "Indeterminado"
+        company.payment_frequency = 8
+      elsif temp == "Anual"
+        company.payment_frequency = 6
+      elsif temp == "Semestral"
+        company.payment_frequency = 5
+      else
+      end
+
+      temp = row[col += 1]
+      if temp == "Indeterminado"
+        company.payment_period = 0
+      elsif temp.is_a? Numeric
+        company.payment_period = temp
+      end
+
+
+      company.first_parcel = row[col += 1]
+
+      # Ignores 'Ultima parcela'
+      col += 1
+
+      temp = row[col += 1]
+      if temp == "Contrato"
+        company.contract = 1
+      elsif temp == "Sem contrato"
+        company.contract = 2
+      end
 
       company
   end
