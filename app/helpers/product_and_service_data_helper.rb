ce3eb593a01c6df2c5e8a95861dc55a434e3f665
Fabiow <fquixada@yahoo.com.br>
2016-05-13 17:59:03 -0300

    P&S: Correcao da Habilitacao dos Campos de Dados Totais

--- app/helpers/product_and_service_data_helper.rb
+++ app/helpers/product_and_service_data_helper.rb
@@ -4,8 +4,8 @@ module ProductAndServiceDataHelper
 		totals_week_number = 6
 		final_week_number = 7
 
-		(week.number == totals_week_number or 
-		(week.product_and_service_datum and (week.product_and_service_datum.finalized? or week.product_and_service_datum.on_analysis?)))
+		week.number == totals_week_number or 
+		(week.product_and_service_datum and (week.product_and_service_datum.finalized? or (week.product_and_service_datum.on_analysis? and week.number != final_week_number)))
 	end
 
 	def week_range_readonly?(week)
