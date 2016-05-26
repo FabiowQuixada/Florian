65f50f6178db959816157fdf4282ba90e8b82aff
Fabiow <fquixada@yahoo.com.br>
2016-05-26 17:18:38 -0300

    Teste de Modelo: Configuracoes do Sistema

---
+++ spec/models/system_setting_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe SystemSetting, :type => :model do
+
+  it { should validate_presence_of(:pse_recipients_array).with_message I18n.t('errors.system_setting.recipients') }
+  it { should validate_presence_of(:pse_private_recipients_array).with_message I18n.t('errors.system_setting.private_recipients') }
+  it { should validate_presence_of(:re_title) }
+  it { should validate_presence_of(:re_body) }
+  it { should validate_presence_of(:pse_title) }
+  it { should validate_presence_of(:pse_body) }
+  it { should validate_presence_of(:pse_day_of_month) }
+
+
+  # Relationships
+  it { should belong_to :user }
+
+end
