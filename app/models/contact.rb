a26dfe0d2bc740746692bc64de223a85b53412cf
Fabiow <fquixada@yahoo.com.br>
2016-06-01 21:27:19 -0300

    Adicao de Restricao de Mantenedora ao Contato

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,11 @@ class Contact < ActiveRecord::Base
   enum contact_type: [ :"Responsável", :"Secretária", :"Setor Financeiro" ]
 
 
+  # Relationships
+  belongs_to :company
+
   # Validations
+  validates :company, presence: true
   validates :telephone, format: { with: PHONE_FORMAT, message: I18n.t('errors.contact.invalid_telephone')}, :allow_blank => true
   validates :celphone, format: { with: PHONE_FORMAT, message: I18n.t('errors.contact.invalid_celphone')}, :allow_blank => true
   validates :fax, format: { with: PHONE_FORMAT, message: I18n.t('errors.contact.invalid_fax')}, :allow_blank => true
