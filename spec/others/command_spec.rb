3bda5f224ff20b3a80355c45a0c81c7771d02c6f
Fabiow <fquixada@yahoo.com.br>
2016-06-04 22:31:58 -0300

    Teste de Busca por Strings (TODO, byebug, etc..)

---
+++ spec/others/command_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe ApplicationController do
+
+  it 'searches for byebug occurrences' do
+  	filename = 'byebug'
+  	command = `git grep byebug ./app > tmp/#{filename}.txt && wc -l tmp/#{filename}.txt`
+    expect(command).to be_empty
+  end
+
+  it 'searches for <<<<<< occurrences' do
+  	filename = 'conflict_start'
+  	command = `git grep "<<<<<<" ./app > tmp/#{filename}.txt && wc -l tmp/#{filename}.txt`
+    expect(command).to be_empty
+  end
+
+  it 'searches for >>>>>> occurrences' do
+  	filename = 'conflict_end'
+  	command = `git grep ">>>>>" ./app > tmp/#{filename}.txt && wc -l tmp/#{filename}.txt`
+    expect(command).to be_empty
+  end
+
+  it 'searches for TODO occurrences' do
+  	filename = 'TODO'
+  	command = `git grep TODO ./app > tmp/#{filename}.txt && wc -l tmp/#{filename}.txt`
+    # expect(command).to be_empty
+  end
+
+end
