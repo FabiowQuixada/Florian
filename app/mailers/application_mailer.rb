0be699701b2c21049daab98b0437638be6b8ac4e
Fabiow <fquixada@yahoo.com.br>
2016-06-25 19:07:09 -0300

    Refatoracao e Reorganizacao de Mailers: Rubocop

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,13 +4,13 @@ class ApplicationMailer < ActionMailer::Base
 
   def send_backup_email
 
-    # TODO: Endereço dinamico q tem no server_up.sh
+    # TODO: Endereco dinamico q tem no server_up.sh
     attachments['backup.tar'] = File.read(Dir['/home/fabiow/backups/db_backup/*'].sort.reverse[1] + '/db_backup.tar')
 
     # TODO: Porque deliver now da erro aqui?
     mail(to: ADMIN_EMAIL,
          body: 'Banco de Producao',
          subject: I18n.t('helpers.email_tag') + ' Backup Semanal') # .deliver_now
-end
+  end
 
 end
--- app/mailers/florian_mailer.rb
+++ app/mailers/florian_mailer.rb
@@ -1,7 +1,7 @@
 class FlorianMailer < ApplicationMailer
 
-  def send_test_receipt_email(email, date = nil, user)
-    send_email(email, date, user, EmailHistory.send_types[:test], I18n.t('helpers.test_email_prefix'), user.email)
+  def send_test_receipt_email(email, user, date = nil)
+    send_email(email, date, user, EmailHistory.send_types[:test], user.email)
   end
 
   def resend_receipt_email(email, date, user)
@@ -14,10 +14,9 @@ class FlorianMailer < ApplicationMailer
 
   def send_weekly_prod_and_serv_email(week, user)
 
-    ProductAndServiceReport.new('/tmp/prod_serv.pdf', week).save
-    attachments['relatorio_de_produtos_e_servicos.pdf'] = File.read('/tmp/prod_serv.pdf')
+    attach_report week
 
-    period = week.start_date.to_s + ' ' + I18n.t('helpers.to') + ' ' + week.end_date.to_s
+    period = week_period week
 
     mail(to: user.system_setting.pse_private_recipients_array,
          subject: SSETTINGS_PSE_TITLE_PREFIX + period,
@@ -26,8 +25,7 @@ class FlorianMailer < ApplicationMailer
 
   def send_prod_and_serv_to_analysis(week, user)
 
-    ProductAndServiceReport.new('/tmp/prod_serv.pdf', week).save
-    attachments['relatorio_de_produtos_e_servicos.pdf'] = File.read('/tmp/prod_serv.pdf')
+    attach_report week
 
     mail(to: ANALYSIS_EMAIL,
          body: user.system_setting.pse_processed_body(week.product_and_service_datum.competence),
@@ -36,28 +34,49 @@ class FlorianMailer < ApplicationMailer
 
   def send_monthly_prod_and_serv_email(week, user)
 
-    ProductAndServiceReport.new('/tmp/prod_serv.pdf', week).save
-    attachments['relatorio_de_produtos_e_servicos.pdf'] = File.read('/tmp/prod_serv.pdf')
+    attach_report week
 
     mail(to: user.system_setting.pse_recipients_array,
          body: user.system_setting.pse_processed_body(week.product_and_service_datum.competence),
          subject: user.system_setting.pse_processed_title(week.product_and_service_datum.competence))
   end
 
-
   private
 
-  def send_email(email, date, user, type, prefix = '', recipients = nil)
+  def send_email(email, date, user, type, recipients = nil)
 
     recipients = email.recipients_array if recipients.nil?
 
-    ReceiptReport.new('/tmp/recibo.pdf', email, date).save
-    attachments['recibo_de_doacao.pdf'] = File.read('/tmp/recibo.pdf')
+    attach_receipt email, date
+
+    prefix = ''
+    prefix = I18n.t('helpers.test_email_prefix') if type == EmailHistory.send_types[:test]
 
     mail(to: recipients,
          body: email.processed_body(user, date),
          subject: prefix + email.processed_title(user, date))
 
+    save_to_history email, type, user
+
+  end
+
+  def send_test_email(email, date, user)
+    mail(to: user.email,
+         body: email.processed_body(user, date),
+         subject: I18n.t('helpers.test_email_prefix') + email.processed_title(user, date))
+  end
+
+  def attach_report(week)
+    ProductAndServiceReport.new('/tmp/prod_serv.pdf', week).save
+    attachments['relatorio_de_produtos_e_servicos.pdf'] = File.read('/tmp/prod_serv.pdf')
+  end
+
+  def attach_receipt(email, date)
+    ReceiptReport.new('/tmp/recibo.pdf', email, date).save
+    attachments['recibo_de_doacao.pdf'] = File.read('/tmp/recibo.pdf')
+  end
+
+  def save_to_history(email, type, user)
     email.email_histories.create(
       receipt_email: email,
       body: email.body,
@@ -68,10 +87,8 @@ class FlorianMailer < ApplicationMailer
     )
   end
 
-  def send_test_email(email, date, user)
-    mail(to: user.email,
-         body: email.processed_body(user, date),
-         subject: I18n.t('helpers.test_email_prefix') + email.processed_title(user, date))
+  def week_period(week)
+    week.start_date.to_s + ' ' + I18n.t('helpers.to') + ' ' + week.end_date.to_s
   end
 
 end
