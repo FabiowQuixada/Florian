4473ed426c5a43bb2610693ab143990dfff13ce8
Fabiow <fquixada@yahoo.com.br>
2016-06-25 19:08:02 -0300

    Refatoracao e Reorganizacao de Parse de Xls: Rubocop

--- app/others/excel_parser.rb
+++ app/others/excel_parser.rb
@@ -3,21 +3,27 @@ class ExcelParser
 
     inconsistent_companies = []
 
-    file = Roo::Excel.new(Rails.root.join('app', 'assets', 'empresas_2016.xls'))
+    parse Roo::Excel.new(Rails.root.join('app', 'assets', 'empresas_2016.xls'))
 
-    file.worksheets.each_with_index do |sheet, index|
+    print_parse_errors inconsistent_companies
+
+    inconsistent_companies
+  end
+
+  def parse(file)
+    file.worksheets.each do |sheet|
       # Get companies
-      for r in 4..(sheet.count - 1)
-
+      (4..(sheet.count - 1)).each do |r|
         row = sheet.rows[r]
 
-        company = parse_company row, (index + 1)
+        company = parse_company row
 
         inconsistent_companies << company unless company.save
       end # Row loop end
     end # Sheet loop end
+  end
 
-
+  def print_parse_errors(inconsistent_companies)
     inconsistent_companies.each do |company|
       puts company
 
@@ -25,148 +31,140 @@ class ExcelParser
         puts error_message
       end
     end
+  end
 
-    inconsistent_companies
-     end
+  def self.parse_company(row)
 
-  private
+    company = Company.new group: 1
 
-  def self.parse_company(row, _group)
+    parse_basic_info company, col = 0
+    parse_general_info company, row, col
+    parse_category company, row[col += 1]
+    parse_cpf_cnpj company, row[col += 1]
+    parse_general_info company, row, col
+    parse_contacts company
+    parse_donation company, row[col += 1]
+    parse_payment_frequency company, row[col += 1]
+    parse_payment_period(company, row[col += 1])
+    company.first_parcel = row[col += 1]
+    parse_contract company, row[col + 1]
 
-    col = 0
-    company = Company.new
+    company
+  end
 
+  def parse_cpf_cnpj(company, val)
+    if company.person?
+      company.cpf = val
+    else
+      company.cnpj = val
+    end
+  end
+
+  def parse_contract(company, val)
+    if val == 'Contrato'
+      company.contract = Company.contracts[:"Com contrato"]
+    elsif val == 'Sem contrato'
+      company.contract = Company.contracts[:"Sem contrato"]
+    end
+  end
+
+  def parse_donation(_company, val)
+    donation = Donation.new
+
+    if val.is_a? Numeric
+      donation.value = val
+    else
+      donation.remark = val
+    end
+  end
+
+  def parse_basic_info(company, row, col)
     company.entity_type = if row[col] == 'PF'
                             Company.entity_types[:"Pessoa Física"]
                           else
                             Company.entity_types[:"Pessoa Jurídica"]
                           end
 
+
+    company.registration_name = row[col += 1]
+    company.name = row[col + 1]
+  end
+
+  def parse_contacts(company)
+    company.contacts.clear
+    parse_contact(company, 0, 12)
+    parse_contact(company, 1, 18)
+    parse_contact(company, 2, 24)
+  end
+
+  def parse_contact(company, type, col)
+
+    contact = Contact.new
+
+    contact.name = row[col += 1]
+    contact.position = row[col += 1]
+    contact.position = 'Secretária' if type == 1
+    contact.position = 'Setor financeiro' if type == 2
     col += 1
-    company.registration_name = row[col]
-    company.name = row[col += 1]
-
-    cat = 0
+    contact.telephone = '85' + row[col].to_s unless row[col].nil?
     col += 1
-    if row[col] == 'I'
-      cat = Company.categories[:"1 (Abaixo de R$ 300,00)"]
-    elsif row[col] == 'II'
-      cat = Company.categories[:"2 (Entre R$ 300,00 e R$ 600,00)"]
-    elsif row[col] == 'III'
-      cat = Company.categories[:"3 (Acima de R$ 600,00)"]
-    end
+    contact.fax = '85' + row[col].to_s unless row[col].nil?
+    col += 1
+    contact.celphone = '85' + row[col].to_s unless row[col].nil?
+    contact.email_address = row[col + 1]
+    contact.contact_type = type
 
+    company.contacts.push contact
 
-    company.category = cat
-    company.group = 1
+  end
 
-    if company.person?
-      company.cpf = row[col += 1]
-    else
-      company.cnpj = row[col += 1]
-    end
-
+  def parse_general_info(company, col)
     company.address = row[col += 1]
     company.neighborhood = row[col += 1]
     company.cep = row[col += 1]
     company.city = row[col += 1]
     company.state = row[col += 1]
     company.email_address = row[col += 1]
-    company.website = row[col += 1]
+    company.website = row[col + 1]
+  end
 
-
-    # Contacts
-    col = 12
-
-    contact1 = Contact.new
-
-    contact1.name = row[col += 1]
-    contact1.position = row[col += 1]
-    col += 1
-    contact1.telephone = '85' + row[col].to_s unless row[col].nil?
-    col += 1
-    contact1.fax = '85' + row[col].to_s unless row[col].nil?
-    col += 1
-    contact1.celphone = '85' + row[col].to_s unless row[col].nil?
-    contact1.email_address = row[col += 1]
-    contact1.contact_type = 0
-
-    contact2 = Contact.new
-    contact2.name = row[col += 1]
-    contact2.position = 'Secretária'
-    col += 1
-    contact2.telephone = '85' + row[col].to_s unless row[col].nil?
-    col += 1
-    contact2.celphone = '85' + row[col].to_s unless row[col].nil?
-    contact2.email_address = row[col += 1]
-    contact2.contact_type = 1
-
-    col += 1
-    contact3 = Contact.new
-    contact3.name = row[col += 1]
-    contact3.position = 'Setor financeiro'
-    col += 1
-    contact3.telephone = '85' + row[col].to_s unless row[col].nil?
-    col += 1
-    contact3.celphone = '85' + row[col].to_s unless row[col].nil?
-    contact3.email_address = row[col += 1]
-    contact3.contact_type = 2
-
-    company.contacts.clear
-    company.contacts.push contact1
-    company.contacts.push contact2
-    company.contacts.push contact3
-    contact1.company = company
-    contact2.company = company
-    contact3.company = company
-
-    # Donation
-    donation = Donation.new
-    temp = row[col += 1]
-
-    if temp.is_a? Numeric
-      donation.value = temp
-    else
-      donation.remark = temp
-    end
-
-    temp = row[col += 1]
-    if temp == 'Mensal'
+  def parse_payment_frequency(company, val)
+    if val == 'Mensal'
       company.payment_frequency = Company.payment_frequencies[:Mensal]
-    elsif temp == 'Diariamente'
+    elsif val == 'Diariamente'
       company.payment_frequency = Company.payment_frequencies[:"Diário"]
-    elsif temp == 'Bimestral'
+    elsif val == 'Bimestral'
       company.payment_frequency = Company.payment_frequencies[:Bimestral]
-    elsif temp == 'Semanal'
+    elsif val == 'Semanal'
       company.payment_frequency = Company.payment_frequencies[:Semanal]
-    elsif temp == 'Indeterminado'
+    elsif val == 'Indeterminado'
       company.payment_frequency = Company.payment_frequencies[:Indeterminado]
-    elsif temp == 'Anual'
+    elsif val == 'Anual'
       company.payment_frequency = Company.payment_frequencies[:Anual]
-    elsif temp == 'Semestral'
+    elsif val == 'Semestral'
       company.payment_frequency = Company.payment_frequencies[:Semestral]
-       end
+    end
+  end
 
-    temp = row[col += 1]
-    if temp == 'Indeterminado'
+  def parse_payment_period(company, val)
+    if val == 'Indeterminado'
       company.payment_period = 0
-    elsif temp.is_a? Numeric
-      company.payment_period = temp
+    elsif val.is_a? Numeric
+      company.payment_period = val
+    end
+  end
+
+  def parse_category(company, val)
+    cat = 0
+
+    if val == 'I'
+      cat = Company.categories[:"1 (Abaixo de R$ 300,00)"]
+    elsif val == 'II'
+      cat = Company.categories[:"2 (Entre R$ 300,00 e R$ 600,00)"]
+    elsif val == 'III'
+      cat = Company.categories[:"3 (Acima de R$ 600,00)"]
     end
 
-
-    company.first_parcel = row[col += 1]
-
-    # Ignores 'Ultima parcela'
-    col += 1
-
-    temp = row[col += 1]
-    if temp == 'Contrato'
-      company.contract = Company.contracts[:"Com contrato"]
-    elsif temp == 'Sem contrato'
-      company.contract = Company.contracts[:"Sem contrato"]
-    end
-
-    company
-   end
+    company.category = cat
+  end
 end
