42a8abf2c91ffcd9b70f919226dd5ac93c56c037
Fabiow <fquixada@yahoo.com.br>
2016-06-25 19:07:39 -0300

    Refatoracao e Reorganizacao de Relatorios: Rubocop

--- app/pdfs/product_and_service_report.rb
+++ app/pdfs/product_and_service_report.rb
@@ -1,63 +1,71 @@
 class ProductAndServiceReport < FlorianReport
 
-  def initialize(path = nil, week)
+  def initialize(path, week)
     @path = path
     @week = week
   end
 
   def pdf
     Prawn::Document.new(PDF_OPTIONS) do |pdf|
-      if @week.number != 7
-        title = I18n.t('report.title.weekly_prod_and_service')
-        subtitle = (@week.start_date.to_s + ' - ' + @week.end_date.to_s)
-      else
-        title = I18n.t('report.title.monthly_prod_and_service')
-        subtitle = I18n.localize(@week.product_and_service_datum.competence, format: :competence).capitalize
-      end
+      render_header pdf, @week
 
-      header pdf, title, subtitle
+      render_service_data pdf
 
-      print_session_title pdf, I18n.t('helpers.prod_and_serv_datum.attendances')
-
-      table = []
-      total_a = 0
-      total_b = 0
-
-      table << [I18n.t('activerecord.models.service_datum.other'), I18n.t('activerecord.attributes.product_and_service_datum.attendances'), I18n.t('activerecord.attributes.product_and_service_datum.returns'), I18n.t('helpers.total')]
-      ServiceData.services.each do |service|
-        a = @week.service_data[0].instance_eval(service)
-        b = @week.service_data[1].instance_eval(service)
-        total_a += a
-        total_b += b
-
-        table << [I18n.t('activerecord.attributes.service_datum.' + service), a.to_s, b.to_s, (a + b).to_s]
-      end
-
-      table << [I18n.t('helpers.total'), total_a, total_b, total_a + total_b]
-
-      print_table pdf, table
-
-
-      ##################
-
-      print_session_title pdf, I18n.t('helpers.prod_and_serv_datum.product_output')
-
-
-      table = []
-      total_products = 0
-      table << [I18n.t('activerecord.models.product_datum.other'), I18n.t('helpers.total')]
-      ProductData.products.each do |product|
-        a = @week.product_data.instance_eval(product)
-        total_products += a
-        table << [I18n.t('activerecord.attributes.product_datum.' + product), a.to_s]
-      end
-
-      table << [I18n.t('helpers.total'), total_products]
-
-      print_table pdf, table
+      render_product_data pdf
 
       footer pdf
     end
+  end
+
+  def render_header(pdf, week)
+    if week.number != 7
+      title = I18n.t('report.title.weekly_prod_and_service')
+      subtitle = (week.start_date.to_s + ' - ' + week.end_date.to_s)
+    else
+      title = I18n.t('report.title.monthly_prod_and_service')
+      subtitle = I18n.localize(week.product_and_service_datum.competence, format: :competence).capitalize
+    end
+
+    header pdf, title, subtitle
+  end
+
+  def render_service_data(pdf)
+    print_session_title pdf, I18n.t('helpers.prod_and_serv_datum.attendances')
+
+    table = []
+    total_a = 0
+    total_b = 0
+
+    table << [I18n.t('activerecord.models.service_datum.other'), I18n.t('activerecord.attributes.product_and_service_datum.attendances'), I18n.t('activerecord.attributes.product_and_service_datum.returns'), I18n.t('helpers.total')]
+    ServiceData.services.each do |service|
+      a = @week.service_data[0].instance_eval(service)
+      b = @week.service_data[1].instance_eval(service)
+      total_a += a
+      total_b += b
+
+      table << [I18n.t('activerecord.attributes.service_datum.' + service), a.to_s, b.to_s, (a + b).to_s]
+    end
+
+    table << [I18n.t('helpers.total'), total_a, total_b, total_a + total_b]
+
+    print_table pdf, table
+  end
+
+  def render_product_data(pdf)
+    print_session_title pdf, I18n.t('helpers.prod_and_serv_datum.product_output')
+
+    table = []
+    total_products = 0
+    table << [I18n.t('activerecord.models.product_datum.other'), I18n.t('helpers.total')]
+    ProductData.products.each do |product|
+      a = @week.product_data.instance_eval(product)
+      total_products += a
+      table << [I18n.t('activerecord.attributes.product_datum.' + product), a.to_s]
+    end
+
+    table << [I18n.t('helpers.total'), total_products]
+
+    print_table pdf, table
   end
 
 end
--- app/pdfs/receipt_report.rb
+++ app/pdfs/receipt_report.rb
@@ -1,45 +1,59 @@
 class ReceiptReport < FlorianReport
 
-  def initialize(path = nil, email, date)
+  def initialize(path, email, date)
     @path = path
     @email = email
     @date = date
-    end
+  end
 
   def pdf
     Prawn::Document.new(PDF_OPTIONS) do |pdf|
-      # Header
       header pdf, I18n.t('report.title.receipt')
 
-      # Value
-      pdf.move_down 30
-      pdf.text ActionController::Base.helpers.number_to_currency(@email.value), size: 18, style: :bold, align: :right
+      render_value pdf
 
-      # Main text
-      pdf.move_down 30
-      pdf.text @email.processed_receipt_text(@date), inline_format: true, align: :justify, indent_paragraphs: 30
+      render_main_text pdf
 
-      # Bank account
-      pdf.move_down 30
-      pdf.indent(20) do
-        pdf.text "DADOS PARA DEPÓSITO", inline_format: true, style: :bold
-        pdf.text "Banco: Itaú (341)", inline_format: true
-        pdf.text "Agência: 8373", inline_format: true
-        pdf.text 'Conta Corrente: 14373-7', inline_format: true
-      end
+      render_bank_data pdf
 
-      # Place / time
-      pdf.move_down 30
-      pdf.text 'Fortaleza, ' + I18n.localize(Date.today, format: :long), inline_format: true, align: :center
+      render_date_place pdf
 
-      # Signature
-      pdf.move_down 30
-      pdf.image "#{Rails.root}/app/assets/images/president_signature.png", scale: 0.15, position: :center
-      pdf.move_up 15
-      pdf.text I18n.t('report.president_name'), inline_format: true, style: :bold, align: :center
-      pdf.text I18n.t('report.president_description'), inline_format: true, style: :bold, align: :center
+      render_signature pdf
 
       footer pdf
     end
+  end
+
+  def render_value(pdf)
+    pdf.move_down 30
+    pdf.text ActionController::Base.helpers.number_to_currency(@email.value), size: 18, style: :bold, align: :right
+  end
+
+  def render_main_text(pdf)
+    pdf.move_down 30
+    pdf.text @email.processed_receipt_text(@date), inline_format: true, align: :justify, indent_paragraphs: 30
+  end
+
+  def render_bank_data(pdf)
+    pdf.move_down 30
+    pdf.indent(20) do
+      pdf.text "DADOS PARA DEPÓSITO", inline_format: true, style: :bold
+      pdf.text "Banco: Itaú (341)", inline_format: true
+      pdf.text "Agência: 8373", inline_format: true
+      pdf.text 'Conta Corrente: 14373-7', inline_format: true
+    end
+  end
+
+  def render_date_place(pdf)
+    pdf.move_down 30
+    pdf.text 'Fortaleza, ' + I18n.localize(Date.today, format: :long), inline_format: true, align: :center
+  end
+
+  def render_signature(pdf)
+    pdf.move_down 30
+    pdf.image "#{Rails.root}/app/assets/images/president_signature.png", scale: 0.15, position: :center
+    pdf.move_up 15
+    pdf.text I18n.t('report.president_name'), inline_format: true, style: :bold, align: :center
+    pdf.text I18n.t('report.president_description'), inline_format: true, style: :bold, align: :center
   end
 end
