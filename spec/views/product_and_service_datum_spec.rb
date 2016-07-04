a22f51163faac9c3e7638c06f038528192162723
Fabiow <fquixada@yahoo.com.br>
2016-07-03 23:26:44 -0300

    Eliminacao da View "_index_table"

--- spec/views/product_and_service_datum_spec.rb
+++ spec/views/product_and_service_datum_spec.rb
@@ -17,7 +17,7 @@ describe 'product_and_service_data/index', type: :view do
     # Others
     expect(rendered).to include 'class="table table-striped table-hover table-bordered table-condensed tablesorter tablesorter-blue"'
     expect(rendered).to include '<tr id="model_'
-    expect(rendered).to include '<td class="model_id admin-only" style="display: none;">'
+    expect(rendered).to include '<td class="model_id admin-only">'
     expect(rendered).to include '<thead>'
     expect(rendered).to include '<tbody>'
   end
@@ -36,7 +36,7 @@ describe 'product_and_service_data/index', type: :view do
     expect(rendered).to include genderize_tag(model, 'helpers.none_registered')
     expect(rendered).to include 'class="table table-striped table-hover table-bordered table-condensed tablesorter tablesorter-blue"'
     expect(rendered).not_to include '<tr id=\'model_'
-    expect(rendered).not_to include '<td class="model_id admin-only" style="display: none;">'
+    expect(rendered).not_to include '<td class="model_id admin-only">'
     expect(rendered).to include '<thead>'
     expect(rendered).to include '<tbody>'
   end
