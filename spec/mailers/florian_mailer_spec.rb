b5fe2a959416c673f1883c593a2ef657882fdd75
Fabiow <fquixada@yahoo.com.br>
2016-07-13 00:32:25 -0300

    Renomecao do Arquivo de Teste do FlorianMailer

--- spec/mailers/florian_mailer_spec.rb
+++ spec/mailers/florian_mailer_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe ProductAndServiceDatum, type: :mailer do
+describe FlorianMailer, type: :mailer do
   let(:week) { create :product_and_service_week }
   let(:user) { build :user, :common }
 
   describe 'weekly e-mail' do
     let(:period) { week.start_date.to_s + ' ' + I18n.t('helpers.to') + ' ' + week.end_date.to_s }
-    let(:mail) { FlorianMailer.send_weekly_prod_and_serv_email(week, user).deliver_now }
+    let(:mail) { described_class.send_weekly_prod_and_serv_email(week, user).deliver_now }
 
     it_behaves_like 'an psd e-mail'
     it { expect(mail.to).to eq(user.system_setting.private_recipients_as_array) }
@@ -16,7 +16,7 @@ describe ProductAndServiceDatum, type: :mailer do
 
   describe 'analysis e-mail' do
     let(:competence) { week.product_and_service_datum.competence }
-    let(:mail) { FlorianMailer.send_prod_and_serv_to_analysis(week, user).deliver_now }
+    let(:mail) { described_class.send_prod_and_serv_to_analysis(week, user).deliver_now }
 
     it_behaves_like 'an psd e-mail'
     it { expect(mail.to).to eq([ANALYSIS_EMAIL]) }
@@ -26,7 +26,7 @@ describe ProductAndServiceDatum, type: :mailer do
 
   describe 'client e-mail' do
     let(:competence) { week.product_and_service_datum.competence }
-    let(:mail) { FlorianMailer.send_monthly_prod_and_serv_email(week, user).deliver_now }
+    let(:mail) { described_class.send_monthly_prod_and_serv_email(week, user).deliver_now }
 
     it_behaves_like 'an psd e-mail'
     it { expect(mail.to).to eq(user.system_setting.recipients_as_array) }
