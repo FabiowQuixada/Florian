604b9ed80ae30e71731ac9514ab1e94a7f2ba442
Fabiow <fquixada@yahoo.com.br>
2016-08-01 15:50:08 -0300

    Refatoracao dos Testes de Dados Nao-Salvos

--- spec/features/unsaved_data_spec.rb
+++ spec/features/unsaved_data_spec.rb
@@ -1,38 +1,44 @@
 require 'rails_helper'
 
-describe 'Unsaved data', type: :request do
-  UNSAVED_DATA = [Company, Bill, ProductAndServiceDatum, Donation, Role, User, ReceiptEmail].freeze
+describe 'Unsaved data', js: true, type: :request do
+  UNSAVED_DATA = [Company, Bill, ProductAndServiceDatum, Donation, ReceiptEmail].freeze
+
+  before :each do
+    login_as_admin
+  end
 
   it 'displays unsaved data warning' do
-    login_as_admin
-
     UNSAVED_DATA.each do |data|
-      fill_non_temp_data_and_return data.new
+      fill_non_temp_data data.new
+      go_back
       expect(page).to have_content('Dados não salvos'), data.name
     end
   end
 
   it 'does not display unsaved data warning if temporary fields are filled' do
-    login_as_admin
-
     [Company, Bill, ProductAndServiceDatum, Donation, ReceiptEmail].each do |data|
-      fill_temp_data_and_return data.new
+      fill_temp_data data.new
+      go_back
       expect(page).to have_no_content('Dados não salvos'), data.name
     end
   end
 
   # == Helper methods =============================================================
 
-  def fill_temp_data_and_return(model)
-
+  def fill_temp_data(model)
     visit edit_path(model)
-
     go_somewhere_with_a_temp_field model
 
     unless all('.temp_field').empty?
       fill_temp_inputs
-      go_back
     end
+  rescue Capybara::ElementNotFound => e
+    raise Capybara::ElementNotFound, e.message + ': ' + model.class.name
+  end
+
+  def fill_non_temp_data(model)
+    visit edit_path(model)
+    fill_non_temp_inputs
   rescue Capybara::ElementNotFound => e
     raise Capybara::ElementNotFound, e.message + ': ' + model.class.name
   end
@@ -41,65 +47,45 @@ describe 'Unsaved data', type: :request do
     page.find('#main_tab_1_title').click if model.is_a? Company
   end
 
-  def fill_non_temp_data_and_return(model)
-    visit edit_path(model)
-    fill_all_non_temp_inputs
-    go_back
-  rescue Capybara::ElementNotFound => e
-    raise Capybara::ElementNotFound, e.message + ': ' + model.class.name
-  end
-
   def edit_path(model)
     send('edit_' + model.class.name.singularize.underscore + '_path', model.class.first.id)
   end
 
   def fill_temp_inputs
-    all('.temp_field').each do |input|
-      input.set 'whatever' unless input[:readonly]
-    end
-  end
-
-  def fill_all_non_temp_inputs
-    fill_non_temp_inputs
-    fill_non_temp_textareas
-    fill_non_temp_dates
-    fill_non_temp_money_inputs
+    field = first('.temp_field:not([readonly])')
+    field.set 'whatever' unless field.nil?
   end
 
   def fill_non_temp_inputs
-    all('input').each do |input|
-      unless input[:readonly]
-        input.set 'whatever'
-        break
-      end
-    end
+    fill_a_non_temp_input
+    fill_a_non_temp_number
+    fill_a_non_temp_textarea
+    fill_a_non_temp_date
+    fill_a_non_temp_money_input
   end
 
-
-  def fill_non_temp_textareas
-    all('textarea').each do |textarea|
-      unless textarea[:readonly]
-        textarea.set 'whatever'
-        break
-      end
-    end
+  def fill_a_non_temp_input
+    field = first('input:not([readonly])')
+    field.set 'whatever' unless field.nil?
   end
 
-  def fill_non_temp_dates
-    all('.date input').each do |date_field|
-      unless date_field[:readonly]
-        date_field.set '11/11/1111'
-        break
-      end
-    end
+  def fill_a_non_temp_number
+    field = first('.numbers_only:not([readonly])')
+    field.set '4' unless field.nil?
   end
 
-  def fill_non_temp_money_inputs
-    all('.money').each do |money_field|
-      unless money_field[:readonly]
-        money_field.set '0,01'
-        break
-      end
-    end
+  def fill_a_non_temp_textarea
+    field = first('textarea:not([readonly])')
+    field.set 'whatever' unless field.nil?
+  end
+
+  def fill_a_non_temp_date
+    field = first('.date:not([readonly])')
+    field.set '11/11/1111' unless field.nil?
+  end
+
+  def fill_a_non_temp_money_input
+    field = first('.money:not([readonly])')
+    field.set '0,01' unless field.nil?
   end
 end
