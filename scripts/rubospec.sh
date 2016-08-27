29a2f4ebd9ba639ace96000b2c03e015195c49d1
Fabiow <fquixada@yahoo.com.br>
2016-08-27 13:16:19 -0300

    Exibicao de Quantidade de TODOs e Desabilitacoes do Rubocop no Script de Teste

--- scripts/rubospec.sh
+++ scripts/rubospec.sh
@@ -1,4 +1,6 @@
 set -e
 bundle exec rubocop -a
 bundle exec rspec
+echo 'Number of rubocop disables: ' && git grep "# rubocop:disable" | wc -l
+echo 'Number of TODOs: ' && git grep "TODO" | wc -l
 git gui
