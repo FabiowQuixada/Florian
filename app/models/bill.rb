7f704e788b35174d63d76b682c05249e575d3a3b
Fabiow <fquixada@yahoo.com.br>
2016-08-31 01:30:36 -0300

    Correcao da Unicidade de Competencia de Conta

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -10,7 +10,7 @@ class Bill < ActiveRecord::Base
 
 
   # Validations
-  validates :competence, presence: true, uniqueness: true
+  validates :competence, presence: true
   validates :water, :energy, :telephone, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validate :validate_model
 
@@ -20,7 +20,7 @@ class Bill < ActiveRecord::Base
     'f'
   end
 
-  private
+  #private #######################################################################################################
 
   def default_values
     self.competence ||= Date.today
@@ -31,5 +31,14 @@ class Bill < ActiveRecord::Base
 
   def validate_model
     self.competence = self.competence.change(day: 1) if self.competence
+
+    attribute = I18n.t('activerecord.attributes.bill.competence')
+    errors.add(:competence, I18n.t('errors.messages.taken', attribute: attribute)) if competence_already_taken?
+  end
+
+  def competence_already_taken?
+   Bill.where("extract(month from competence) = #{competence.month} 
+    AND extract(year from competence) = #{competence.year} " +
+    ( id ? "AND id != #{id}" : "" )).any?
   end
 end
