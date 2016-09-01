d79476f8b139d359c48ed69200992218b09f6063
Fabiow <fquixada@yahoo.com.br>
2016-08-31 22:29:43 -0300

    Script Showcase: Queda do Servidor e Reinicializacao do Banco

--- scripts/showcase_up.sh
+++ scripts/showcase_up.sh
@@ -1,3 +1,4 @@
 # Showcase
-bundle exec rake db:migrate RAILS_ENV=showcase
+kill -9 $(lsof -ti tcp:4000)
+bundle exec rake db:drop db:create db:migrate db:seed RAILS_ENV=showcase
 rails s Puma -b ec2-52-67-11-119.sa-east-1.compute.amazonaws.com -p 4000 -e showcase &
