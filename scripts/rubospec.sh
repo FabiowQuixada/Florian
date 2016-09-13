6b4d5dfa1d63f3be5c70e133a658fb065ec90d8e
Fabiow <fquixada@yahoo.com.br>
2016-09-13 16:28:59 -0300

    Ajuste no Script de Testes: Ignorar Determinados Diretorios

--- scripts/rubospec.sh
+++ scripts/rubospec.sh
@@ -1,6 +1,6 @@
 set -e
 bundle exec rubocop -a
 bundle exec rspec
-echo 'Number of rubocop disables: ' && git grep "# rubocop:disable" | wc -l
-echo 'Number of TODOs: ' && git grep "TODO" | wc -l
+echo 'Number of rubocop disables: ' && git grep "# rubocop:disable" -- './*' ':!/doc/*' ':!/scripts/*' | wc -l
+echo 'Number of TODOs: ' && git grep "TODO: " -- './*' ':!/doc/*' ':!/scripts/*' | wc -l
 git gui
