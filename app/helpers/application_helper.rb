950ba694c68dcb3f5ab56fba89ee0298119b9003
Fabiow <fquixada@yahoo.com.br>
2016-09-15 23:43:53 -0300

    Generalizacao e Centralizacao do Codigo de Tabela sem Registros (View)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -90,6 +90,14 @@ module ApplicationHelper
     ('display_hideless_warning(\'' + t('showcase.login_info', showcase_email: bold('visitante' + GUEST_USERS_NUMBERS.sample.to_s + '@florian.com'), showcase_password: bold(SHOWCASE_PASSWORD)) + '\');').html_safe if showcase_login_screen?
   end
 
+  def no_records_row(model, list, colspan = 20)
+    style = list.any? ? 'display:none' : ''
+
+    content_tag :tr, id: "no_#{model.class.name.pluralize.underscore}_row", style: style do
+      content_tag(:td, genderize_tag(model, 'helpers.none_registered'), colspan: colspan)
+    end
+  end
+
 
   private ############################################################################################
 
