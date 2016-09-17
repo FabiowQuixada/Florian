cf470a3062811fd83e24ae901daf936c057cbc21
Fabiow <fquixada@yahoo.com.br>
2016-09-17 00:04:17 -0300

    Desmembramento da View Principal da Aplicacao

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,35 +33,13 @@ function toogle_admin_data() {
 	display_admin_data = !display_admin_data;
 }
 
-function display_confirm_modal(title, message, confirm_callback, cancel_callback) {
-	
-	$("#confirm_modal .modal-footer").show();
-	$("#cancel_btn").show();
-
-	$("#confirm_modal .modal-title").html(title);
-	$("#confirm_modal .modal-body").html(message);
-	$("#confirm_btn").on("click", confirm_callback);
-	
-	if(cancel_callback !== undefined) {
-		$("#cancel_btn").on("click", cancel_callback);
-	} else {
-		$("#cancel_btn").hide();
-	}
-
-	if(confirm_callback === undefined && cancel_callback === undefined) {
-		$("#confirm_modal .modal-footer").hide();
-	}
-	
-	$("#confirm_modal").modal("show");
-}
-
 function to_top() {
 	$('html, body').animate({
 		scrollTop: $("body").offset().top
 	}, 1000);
 }
 
-var entityMap = {
+var entity_map = {
 	"&": "&amp;",
 	"<": "&lt;",
 	">": "&gt;",
@@ -72,7 +50,7 @@ var entityMap = {
 
 function escape_html(string) {
 	return String(string).replace(/[&<>"'\/]/g, function (s) {
-		return entityMap[s];
+		return entity_map[s];
 	});
 }
 
