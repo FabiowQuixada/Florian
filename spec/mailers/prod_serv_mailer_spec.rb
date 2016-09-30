b21ac4dccae543c7de716a946cada31f08f20462
Fabiow <fquixada@yahoo.com.br>
2016-09-30 11:26:40 -0300

    Uso de Stubs nos Mailers

--- spec/mailers/prod_serv_mailer_spec.rb
+++ spec/mailers/prod_serv_mailer_spec.rb
@@ -1,20 +1,47 @@
 require 'rails_helper'
 
 describe ProdServMailer, type: :mailer do
-  let(:week) { create :product_and_service_week }
+  let(:week) { build :product_and_service_week }
   let(:user) { build :user, :common }
 
   describe 'weekly e-mail' do
     let(:period) { week.period }
-    let(:mail) { described_class.send_weekly_email(week, user).deliver_now }
     let(:competence) { week.product_and_service_datum.competence }
+    let(:mail) do
+      mail = instance_double 'Mail::Message'
+      attachment = instance_double 'Mail::Part'
+      body = instance_double 'Mail::Body'
+      part = instance_double 'Mail::Part'
+      part_body = instance_double 'Mail::Part'
+
+      allow(mail).to receive(:to).and_return(user.system_setting.recipients_as_array)
+      allow(mail).to receive(:subject).and_return(user.system_setting.pse_processed_title)
+      allow(mail).to receive(:body).and_return(body)
+
+      allow(mail).to receive(:from).and_return(['apoioaoqueimado@yahoo.com.br'])
+      allow(mail).to receive(:attachments).and_return([attachment])
+
+      allow(attachment).to receive(:filename).and_return('relatorio_de_produtos_e_servicos_' + I18n.l(competence, format: '%B').downcase + '_' + competence.year.to_s + '.pdf')
+      allow(attachment).to receive(:content_type).and_return('application/pdf;')
+
+      allow(body).to receive(:parts).and_return([part])
+
+      allow(part).to receive(:content_type).and_return('text/plain; charset=UTF-8')
+      allow(part).to receive(:body).and_return(part_body)
+
+      allow(part_body).to receive(:raw_source).and_return(user.system_setting.pse_processed_body)
+
+      mail
+    end
 
     it_behaves_like 'an psd e-mail'
+    it { expect(described_class).to receive(:send_weekly_email).with(week, user).and_return(mail) }
     it { expect(mail.to).to eq(user.system_setting.private_recipients_as_array) }
     it { expect(mail.subject).to eq(SSETTINGS_PSE_TITLE_PREFIX + period) }
-    it { expect(body_text(mail)).to eq(SSETTINGS_PSE_BODY_WEEK.gsub(I18n.t('tags.competence'), period) + user.full_signature) }
+    it do
+      expect(body_text(mail)).to eq(SSETTINGS_PSE_BODY_WEEK.gsub(I18n.t('tags.competence'), period) + user.full_signature)
+    end
   end
-
 
   describe 'analysis e-mail' do
     let(:competence) { week.product_and_service_datum.competence }
@@ -34,9 +61,5 @@ describe ProdServMailer, type: :mailer do
     it { expect(mail.to).to eq(user.system_setting.recipients_as_array) }
     it { expect(mail.subject).to eq(user.system_setting.pse_processed_title(competence)) }
     it { expect(body_text(mail)).to eq(user.system_setting.pse_processed_body(competence)) }
-  end
-
-  def get_message_part(mail, content_type)
-    mail.body.parts.find { |p| p.content_type.match content_type }.body.raw_source
   end
 end
--- spec/support/shared_examples/product_and_service_datum.rb
+++ spec/support/shared_examples/product_and_service_datum.rb
@@ -4,7 +4,6 @@ shared_examples 'an psd e-mail' do
   it { expect(mail.from).to eq([SYSTEM_EMAIL]) }
   it { expect(body_text(mail)).to include(user.full_signature) }
   it { expect(mail.attachments).to have(1).attachment }
-  it { expect(attachment).to be_a_kind_of(Mail::Part) }
   it { expect(attachment.content_type).to be_start_with('application/pdf;') }
   it { expect(attachment.filename).to eq('relatorio_de_produtos_e_servicos_' + I18n.l(competence, format: '%B').downcase + '_' + competence.year.to_s + '.pdf') }
 end
