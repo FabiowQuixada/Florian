b0379473a2a7b31ccdfba03fdac03676697be6c1
Fabiow <fquixada@yahoo.com.br>
2016-10-01 00:48:26 -0300

    Ajustes e Adicoes aos Testes de Internacionalizacao

--- spec/features/locale_spec.rb
+++ spec/features/locale_spec.rb
@@ -1,26 +1,37 @@
 require 'rails_helper'
 
 describe 'Locale', type: :request do
-  LOCALE_DATA = [Company, Donation, ReceiptEmail, ProductAndServiceDatum, User, Role].freeze
+  LOCALE_DATA = [Company, Donation, ReceiptEmail, ProductAndServiceDatum, Bill, User, Role].freeze
 
-  before :each do
-    login_as_admin
+  it 'contains no translation missing on login page' do
+    visit root_path
+    expect(page).to have_no_content 'translation_missing'
   end
 
-  it 'contains no translation missing' do
-    LOCALE_DATA.each do |data|
-      visit send(data.model_name.route_key + '_path')
-      expect(page).to have_no_content('translation_missing'), data.name
+  context 'logged in' do
+    before :each do
+      login_as_admin
     end
-  end
 
-  it 'contains no translation missing' do
-    LOCALE_DATA.each do |data|
-      next unless data.new.insertable
+    it 'contains no translation missing on index page' do
+      LOCALE_DATA.each do |data|
+        visit send("#{data.model_name.route_key}_path")
+        expect(page).to have_no_content('translation_missing'), data.name
+      end
+    end
 
-      visit send('new_' + data.name.singularize.underscore + '_path')
+    it 'contains no translation missing on new page' do
+      LOCALE_DATA.each do |data|
+        visit send("new_#{data.name.singularize.underscore}_path")
+        expect(page).to have_no_content('translation_missing'), data.name
+      end
+    end
 
-      expect(page).to have_no_content('translation_missing'), data.name
+    it 'contains no translation missing on edit page' do
+      LOCALE_DATA.each do |data|
+        visit send("edit_#{data.name.singularize.underscore}_path", data.first)
+        expect(page).to have_no_content('translation_missing'), data.name
+      end
     end
   end
 end
