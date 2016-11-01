35fa0d482c288ec74bc3b40732de66cf843c0ae5
Fabiow <fquixada@yahoo.com.br>
2016-11-01 00:48:13 -0300

    Locale Helper Contextualized by... Locale

--- spec/lib_modules/locale_helper_spec.rb
+++ spec/lib_modules/locale_helper_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 describe LocaleHelper do
-  let(:dummy_class) { Class.new { include LocaleHelper } }
+  context 'pt-BR' do
+    before :all do
+      I18n.locale = :"pt-BR"
+    end
 
-  it { expect(dummy_class.new.genderize_full_tag(Donation.new, 'helpers.action.new')). to eq 'Nova' }
-  it { expect(dummy_class.new.genderize_full_tag(ReceiptEmail.new, 'helpers.action.new')). to eq 'Novo' }
-  it { expect(dummy_class.new.genderize_full_tag(Maintainer.new, 'helpers.action.update_and_new')). to eq 'Salvar e cadastrar nova' }
-  it { expect(dummy_class.new.genderize_full_tag(User.new, 'helpers.action.update_and_new')). to eq 'Salvar e cadastrar novo' }
+    let(:dummy_class) { Class.new { include LocaleHelper } }
+
+    it { expect(dummy_class.new.genderize_full_tag(Donation.new, 'helpers.action.new')). to eq 'Nova' }
+    it { expect(dummy_class.new.genderize_full_tag(ReceiptEmail.new, 'helpers.action.new')). to eq 'Novo' }
+    it { expect(dummy_class.new.genderize_full_tag(Maintainer.new, 'helpers.action.update_and_new')). to eq 'Salvar e cadastrar nova' }
+    it { expect(dummy_class.new.genderize_full_tag(User.new, 'helpers.action.update_and_new')). to eq 'Salvar e cadastrar novo' }
+
+    after :all do
+      I18n.locale = I18n.default_locale
+    end
+  end
 end
