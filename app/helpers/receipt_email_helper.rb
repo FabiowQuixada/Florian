9b3b3c6546d5e0a9a89a7aafd7216c2cc1247e74
Fabiow <fquixada@yahoo.com.br>
2016-12-17 14:34:26 -0300

    Receipt E-mail Test Send Fix

--- app/helpers/receipt_email_helper.rb
+++ app/helpers/receipt_email_helper.rb
@@ -5,7 +5,7 @@ module ReceiptEmailHelper
   end
 
   def send_test_btn
-    link_to image_tag('test_email.png', title: "#{t('description.email.send_test')} '#{current_user.email}'"), 'javascript:void(0)', remote: true, class: 'send_test_btn'
+    link_to image_tag('test_email.png', title: "#{t('description.email.send_test')} '#{current_user.email}'"), 'javascript:void(0)', class: 'send_test_btn'
   end
 
   def recent_emails_btn
