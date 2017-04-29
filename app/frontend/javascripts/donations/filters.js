35a6bb4ad87a2f7ad14a207c4d8ab1869c6d9cbe
Fabiow <ftquixada@gmail.com>
2017-04-29 14:27:55 -0300

    Donations JS Refactoring

--- app/frontend/javascripts/donations/filters.js
+++ app/frontend/javascripts/donations/filters.js
@@ -2,22 +2,28 @@ import { on_page } from './../application'
 import { display_error } from './../message_area'
 import { validate_period, to_rails_date } from './../dates'
 
-$(() => { if(on_page('donations', 'index')) donations_filters() });
+$(() => { if(on_page('donations', 'index')) new DonationsFilters() });
 
-const donations_filters = () => {
-  $('#search_form').on('submit', e => {
-    try { 
-      const start_date = to_rails_date($('#q_donation_date_gteq').val());
-      const end_date = to_rails_date($('#q_donation_date_lteq').val());
-      const errors = validate_period(start_date, end_date);
+const DonationsFilters = (function() {
+  function DonationsFilters() {
+    $('#search_form').on('submit', e => {
+      try { 
+        const start_date = to_rails_date($('#q_donation_date_gteq').val());
+        const end_date = to_rails_date($('#q_donation_date_lteq').val());
+        const errors = validate_period(start_date, end_date);
 
-      if(errors.length !== 0) {
-        display_error(errors, 'filter');
+        if(errors.length !== 0) {
+          display_error(errors, 'filter');
+          e.preventDefault();
+        }
+      } catch(error) {
         e.preventDefault();
+        display_error(error);
       }
-    } catch(error) {
-      e.preventDefault();
-      display_error(error);
-    }
-  });
-}
+    });
+  }
+  
+  return DonationsFilters;
+}());
+
+export default DonationsFilters;
