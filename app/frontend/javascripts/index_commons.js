b9673c4637dce800184fa9f5b66a6ded3027fbfa
Fabiow <ftquixada@gmail.com>
2017-05-02 01:26:47 -0300

    Index Commons JS Refactoring

--- app/frontend/javascripts/index_commons.js
+++ app/frontend/javascripts/index_commons.js
@@ -6,123 +6,134 @@ import ServerFunctions from './server_functions'
 
 $(() => { if(on_action('index')) new IndexCommons() });
 
-class IndexCommons {
-  constructor() {
-    let any_field_filled = false;
+const IndexCommons = (function() {
+  function IndexCommons() {
+    this.setup_filter_panel = () => {
+      let any_field_filled = false;
 
-    $("#index_filters input:not([type='submit']), #index_filters select").each((index, field) => {
-      if (!$(field).val()) {
-        any_field_filled = true;
-        return;
+      $("#index_filters input:not([type='submit']), #index_filters select").each((index, field) => {
+        if (!$(field).val()) {
+          any_field_filled = true;
+          return;
+        }
+      });
+
+      if(any_field_filled) {
+        $('.panel-collapse').collapse({
+          toggle: true
+        });
       }
-    });
-    
-    if(any_field_filled) {
-      $('.panel-collapse').collapse({
-        toggle: true
+    }
+
+    this.setup_listeners = () => {
+      const self = this;
+
+      $('.clean_filters_btn').on('click', () => {
+        const model = $('#rails_model').val();
+        window.location = ServerFunctions.paths.index(model);
+      });
+
+      $('#index_table').on('click', '.status_btn', e => {
+        const id = $(e.currentTarget).closest('.model_row').find('.model_id').html();
+        self.update_status(id);
+      });
+
+      $('.destroy_btn').on('click', e => {
+        const id = $(e.currentTarget).closest('.model_row').find('.model_id').html();
+        const model = $('#rails_model').val();
+        const i18nModel = I18n.t(`activerecord.models.${model}.one`).toLowerCase();
+        display_confirm_modal(I18n.t('modal.title.destroy'),
+          I18n.t('model_phrases.are_you_sure.destroy.n.s', { model: i18nModel }), 
+          self.destroy_model.bind(self, id)
+        );
       });
     }
 
-    $('.clean_filters_btn').on('click', () => {
-      const model = $('#rails_model').val();
-      window.location = ServerFunctions.paths.index(model);
-    });
+    this.update_status = id => {
+      const self = this;
+      const controller = $('#rails_controller').val();
+      const target = `#model_${id} .status > a`;
+      const current_status = $(`#index_table #model_${id} .activate_btn`).length === 0;
 
-    $('#index_table').on('click', '.status_btn', e => {
-      const id = $(e.currentTarget).closest('.model_row').find('.model_id').html();
-      IndexCommons.update_status(id);
-    });
-
-    $('.destroy_btn').on('click', e => {
-      const id = $(e.currentTarget).closest('.model_row').find('.model_id').html();
-      const model = $('#rails_model').val();
-      const i18nModel = I18n.t(`activerecord.models.${model}.one`).toLowerCase();
-      display_confirm_modal(I18n.t('modal.title.destroy'),
-        I18n.t('model_phrases.are_you_sure.destroy.n.s', { model: i18nModel }), 
-        IndexCommons.destroy_model.bind(self, id)
-      );
-    });
-  }
-
-  static update_status(id) {
-    const controller = $('#rails_controller').val();
-    const target = `#model_${id} .status > a`;
-    // TODO
-    const current_status = $(`#index_table #model_${id} .activate_btn`).length;
-
-    $.ajax({
-      type: "POST",
-      url: IndexCommons.status_path(controller, id, current_status),
-      beforeSend: xhr => {
-        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
-      },
-      dataType: "json",
-      success: response => {
-        const message = response.message;
-        const model = $('#rails_model').val();
-        const target = `tr#model_${id} td.status`;
-
-        if(response.status === "ok") {
-          display_notice(message);
-
-          if(response.activated === true) {
-            $(target).html(
-              ServerFunctions.buttons.deactivate(model, response.id)
-            );
-          }
-          else {
-            $(target).html(
-              ServerFunctions.buttons.activate(model, response.id)
-            );
-          }
-        } else {
-          display_error(message);
-        }
-      },
-      error: response => {
-        display_error(I18n.t('error_pages.internal_server_error.title'));
-      }
-    });
-  }
-
-  static status_path(controller, id, is_now_active) {
-    if(is_now_active === 0)
-      return ServerFunctions.paths.deactivate(controller, id);
-    else
-      return ServerFunctions.paths.activate(controller, id);
-  }
-
-  static destroy_model(id) {
-    const controller = $('#rails_controller').val();
-    
-    $.ajax({
-      type: "DELETE",
-      url: `${ServerFunctions.paths.index(controller)}/${id}`,
-      beforeSend: xhr => {
-        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
-      },
-      dataType: "json",
-      success: response => {
-        const message = response.message;
-
-        if(response.success) {
-          display_notice(message);
-
-          $(`#model_${response.model_id}`).remove();
+      $.ajax({
+        type: "POST",
+        url: self.status_path(controller, id, current_status),
+        beforeSend: xhr => {
+          xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+        },
+        dataType: "json",
+        success: response => {
+          const message = response.message;
           const model = $('#rails_model').val();
+          const target = `tr#model_${id} td.status`;
 
-          if($('#index_table tbody tr').length === 1) {
-            $(`tr.no_${model}_row`).removeClass('hidden');
+          if(response.status === "ok") {
+            display_notice(message);
+
+            if(response.activated === true) {
+              $(target).html(
+                ServerFunctions.buttons.deactivate(model, response.id)
+              );
+            }
+            else {
+              $(target).html(
+                ServerFunctions.buttons.activate(model, response.id)
+              );
+            }
+          } else {
+            display_error(message);
           }
-        } else {
-          display_error(message);
+        },
+        error: response => {
+          display_error(I18n.t('error_pages.internal_server_error.title'));
         }
-      },
-      error: response => {
-        display_error(I18n.t('error_pages.internal_server_error.title'));
-      }
-    });
-  }
-}
+      });
+    }
 
-export default IndexCommons
+    this.status_path = (controller, id, is_now_active) => {
+      if(is_now_active)
+        return ServerFunctions.paths.deactivate(controller, id);
+      else
+        return ServerFunctions.paths.activate(controller, id);
+    }
+
+    this.destroy_model = (id) => {
+      const controller = $('#rails_controller').val();
+      
+      $.ajax({
+        type: "DELETE",
+        url: `${ServerFunctions.paths.index(controller)}/${id}`,
+        beforeSend: xhr => {
+          xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+        },
+        dataType: "json",
+        success: response => {
+          const message = response.message;
+
+          if(response.success) {
+            display_notice(message);
+
+            $(`#model_${response.model_id}`).remove();
+            const model = $('#rails_model').val();
+
+            if($('#index_table tbody tr').length === 1) {
+              $(`tr.no_${model}_row`).removeClass('hidden');
+            }
+          } else {
+            display_error(message);
+          }
+        },
+        error: response => {
+          display_error(I18n.t('error_pages.internal_server_error.title'));
+        }
+      });
+    }
+
+    this.setup_listeners();
+    this.setup_filter_panel();
+  }
+
+  return IndexCommons;
+}());
+
+export default IndexCommons;
---
+++ spec/javascripts/index_commons.spec.js
@@ -0,0 +1,64 @@
+import IndexCommons from './../../app/frontend/javascripts/index_commons'
+
+/**
+ *
+ * 
+ * There is some problem when loading 'new IndexCommons();'
+ *
+ *
+**/
+
+// describe("Index commons", () => {
+//   let index_commons;
+
+//   beforeEach(() => {
+//     jasmine.getFixtures().fixturesPath = 'base/spec/javascripts/fixtures';
+//     loadFixtures('index_commons.html');
+//     index_commons = new IndexCommons();
+//     $.holdReady(true);
+//   });
+
+//   describe("setup_filter_panel", () => {
+//     it("expands filter panel if any field is filled", () => {
+//       $('#some_filter').val('');
+//       index_commons.setup_filter_panel();
+//       expect($('#index_filters').attr("aria-expanded")).toEqual('true');
+//     });
+
+//     it("keeps filter panel closed if no field is filled", () => {
+//       $('#some_filter').val('some_value');
+//       index_commons.setup_filter_panel();
+//       expect($('#index_filters').attr("aria-expanded")).toEqual('false');
+//     });
+//   });
+
+//   describe("setup_listeners", () => {
+//     // TODO
+//   });
+
+//   describe("update_status", () => {
+//     // TODO
+//   });
+
+//   describe("status_path", () => {
+//     it("returns the deactivate path for receipt e-mails", () => {
+//       expect(index_commons.status_path('receipt_emails', 2, 0)).toEqual('/receipt_emails/2/deactivate');
+//     });
+
+//     it("returns the activate path for receipt e-mails", () => {
+//       expect(index_commons.status_path('receipt_emails', 3, 1)).toEqual('/receipt_emails/3/activate');
+//     });
+
+//     it("returns the deactivate path for maintainers", () => {
+//       expect(index_commons.status_path('maintainers', 4, 0)).toEqual('/maintainers/4/deactivate');
+//     });
+
+//     it("returns the activate path for maintainers", () => {
+//       expect(index_commons.status_path('maintainers', 5, 1)).toEqual('/maintainers/5/activate');
+//     });
+//   });
+
+//   describe("destroy_model", () => {
+//     // TODO
+//   });
+// });
