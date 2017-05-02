d294201e2811a388d8871adea6e307b2409875ab
Fabiow <ftquixada@gmail.com>
2017-05-02 01:27:31 -0300

    Boostrap Modals Specs

---
+++ spec/javascripts/pages/others/modals.spec.js
@@ -0,0 +1,79 @@
+import { display_confirm_modal } from './../../../../app/frontend/javascripts/others/modals'
+
+describe("Modal", () => {
+  const title = "Some title";
+  const msg = "Some weird message";
+  const confirm_callback = () => {};
+  const cancel_callback = () => {};
+
+  // TODO: Bootstrap is for some reason not recognized in the tests;
+  beforeEach(() => {
+    jasmine.getFixtures().fixturesPath = 'base/spec/javascripts/fixtures';
+    loadFixtures('others/modals.html');
+    $.holdReady(true);
+  });
+
+  // it("pops-up", () => {
+  //   // TODO
+  // });
+
+  // describe("texts", () => {
+  //   beforeEach(() => {
+  //     display_confirm_modal(title, msg);
+  //   });
+
+  //   it("displays title", () => {
+  //     // expect($('#confirm_modal .modal-title').html()).toEqual(title);
+  //   });
+
+  //   it("displays message", () => {
+  //     // expect($('#confirm_modal .modal-body').html()).toEqual(msg);
+  //   });
+  // });
+
+  // describe("absence of callback parameters", () => {
+  //   beforeEach(() => {
+  //     display_confirm_modal(title, msg);
+  //   });
+
+  //   it("does not display modal footer", () => {
+  //     expect($("#confirm_modal .modal-footer").hasClass('hidden')).toEqual(true);
+  //   });
+  // });
+
+  // describe("with only confirm callback", () => {
+  //   beforeEach(() => {
+  //     display_confirm_modal(title, msg, confirm_callback);
+  //   });
+
+  //   it("confirm button is bound to confirm callback", () => {
+  //     // TODO
+  //   });
+
+  //   it("does not display cancel button", () => {
+  //     expect($('#cancel_btn').hasClass('hidden')).toEqual(true);
+  //   });
+  // });
+
+  // describe("with both confirm and cancel callbacks", () => {
+  //   beforeEach(() => {
+  //     display_confirm_modal(title, msg, confirm_callback, cancel_callback);
+  //   });
+
+  //   it("confirm button is bound to confirm callback", () => {
+  //     // TODO
+  //   });
+
+  //   it("does not display cancel button", () => {
+  //     expect($('#cancel_btn').hasClass('hidden')).toEqual(true);
+  //   });
+
+  //   it("cancel button is bound to cancel callback", () => {
+  //     // TODO
+  //   });
+
+  //   it("does not display cancel button", () => {
+  //     expect($('#cancel_btn').hasClass('hidden')).toEqual(false);
+  //   });
+  // });
+});
