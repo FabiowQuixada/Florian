371582bf5117146a63e1802a4f4cb6a614afd7fb
Fabiow <ftquixada@gmail.com>
2017-05-09 15:22:15 -0300

    Receipt E-mail JS Refactoring

--- app/frontend/javascripts/receipt_emails/form.jsx
+++ app/frontend/javascripts/receipt_emails/form.jsx
@@ -4,6 +4,7 @@ import { createStore } from 'redux'
 import { on_page, escape_html } from './../application'
 import { Provider } from 'react-redux';
 import EmailAreaContainer from './../redux/containers/EmailAreaContainer'
+import { clean_resend_modal, clean_send_test_modal } from './modals'
 import Constants from './../server_constants'
 import emailsReducer from './../redux/reducers/emailsReducer';
 import { new_recipients, } from './../others/email_address_table'
@@ -11,77 +12,91 @@ import I18n from './../i18n'
 import { formated_recipients } from '../others/email_address_table'
 import { init, add_tag_to_field } from './../form_commons'
 
-$(() => { if(on_page('receipt_emails', 'form')) receipt_emails_form() });
+$(() => { if(on_page('receipt_emails', 'form')) new ReceiptEmailsForm() });
 
-const receipt_emails_form = () => {
-  const email_field = 'recipients_array';
-  const store = createStore(emailsReducer);
+const ReceiptEmailsForm = (function() {
+  function ReceiptEmailsForm() {
+    const self = this;
+    
+    this.before_submit_or_leave = () => {
+      const email_field = 'recipients_array';
+      $(`#receipt_email_${email_field}`).val(formated_recipients(email_field));
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <EmailAreaContainer 
-        model="receipt_email"
-        label={ I18n.t("activerecord.attributes.receipt_email.recipients_array") } />
-    </Provider>,
-    document.getElementById("email_area")
-  );
+      if(new_recipients(email_field)) {
+        return true;
+      }
 
-  if(on_page('receipt_emails', 'edit')) {
-    $('.resend_btn').click( e => {
-      const id = $("#receipt_email_id").val();
-      const company_name = $('#receipt_email_maintainer > option[selected="selected"]').text();
+      return false;
+    }
 
-      clean_resend_modal();
-      $('.modal_maintainer_name').text(company_name);
-      $('#resend_email_modal').modal('show');
-    });
+    this.validate_tag_fields = () => !this.contains_all_tags($('#receipt_email_body').val())
 
-    $('.send_test_btn').click( e => {
-      const id = $("#receipt_email_id").val();
-      const company_name = $('#receipt_email_maintainer > option[selected="selected"]').text();
+    this.contains_all_tags = text => (
+      text.indexOf(I18n.t('tags.maintainer')) == -1 ||
+      text.indexOf(I18n.t('tags.value')) == -1 ||
+      text.indexOf(I18n.t('tags.competence')) == -1
+    )
+
+    this.setup_listeners = () => {
+      $("#add_maintainer_to_body_btn").on('click', () => add_tag_to_field('receipt_email_body', I18n.t('tags.maintainer')));
+      $("#add_value_to_body_btn").on('click', () => add_tag_to_field('receipt_email_body', I18n.t('tags.value')));
+      $("#add_competence_to_body_btn").on('click', () => add_tag_to_field('receipt_email_body', I18n.t('tags.competence')));
       
-      clean_send_test_modal();
-      $('.modal_maintainer_name').text(company_name);
-      $('#send_test_email_modal').modal('show');
-    });
-  }
+      $('#body_tag_help_btn').click( e => {
+        display_confirm_modal(I18n.t('modal.title.info'), I18n.t('user_help_messages.tag_buttons'));
+      });
 
-  $('#receipt_email_title').val(escape_html(Constants.system_settings.receipt_title));
+      $('#main_form').on('submit', e => {
+        self.before_submit_or_leave();
 
+        if(!self.validate_tag_fields()) {
+          e.preventDefault();
+          $('#warning_save_modal').modal('show');
+          return;
+        }
+      });
 
-  this.before_submit_or_leave = () => {
-    if(new_recipients(email_field)) {
-      return true;
+      if(on_page('receipt_emails', 'edit')) {
+        $('.resend_btn').click( e => {
+          const id = $("#receipt_email_id").val();
+          const company_name = $('#receipt_email_maintainer > option[selected="selected"]').text();
+
+          clean_resend_modal();
+          $('.modal_maintainer_name').text(company_name);
+          $('#resend_email_modal').modal('show');
+        });
+
+        $('.send_test_btn').click( e => {
+          const id = $("#receipt_email_id").val();
+          const company_name = $('#receipt_email_maintainer > option[selected="selected"]').text();
+          
+          clean_send_test_modal();
+          $('.modal_maintainer_name').text(company_name);
+          $('#send_test_email_modal').modal('show');
+        });
+      }
     }
 
-    $(`#receipt_email_${email_field}`).val(formated_recipients(email_field));
+    this.setup_view_components = () => {
+      $('#receipt_email_title').val(escape_html(Constants.system_settings.receipt_title));
+      this.setup_listeners();
 
-    return false;
+      ReactDOM.render(
+        <Provider store={createStore(emailsReducer)}>
+          <EmailAreaContainer 
+            model="receipt_email"
+            label={ I18n.t("activerecord.attributes.receipt_email.recipients_array") } />
+        </Provider>,
+        document.getElementById("email_area")
+      );
+    }
+
+    init(this.before_submit_or_leave);
+    this.setup_listeners();
+    this.setup_view_components();
   }
 
-  init(this.before_submit_or_leave);
+  return ReceiptEmailsForm;
+}());
 
-  $('#body_tag_help_btn').click( e => {
-    display_confirm_modal(I18n.t('modal.title.info'), I18n.t('user_help_messages.tag_buttons'));
-  });
-
-  $('#main_form').on('submit', e => {
-    before_submit_or_leave();
-
-    if(!validate_tag_fields()) {
-      $('#warning_save_modal').modal('show');
-      e.preventDefault();
-      return;
-    }
-  });
-
-  const validate_tag_fields = () => !contains_all_tags($('#receipt_email_body').val())
-
-  const contains_all_tags = text => (
-    text.indexOf(I18n.t('tags.maintainer')) == -1 || text.indexOf(I18n.t('tags.value')) == -1 || text.indexOf(I18n.t('tags.competence')) == -1
-  )
-
-  $("#add_maintainer_to_body_btn").on('click', () => add_tag_to_field('receipt_email_body', I18n.t('tags.maintainer')));
-  $("#add_value_to_body_btn").on('click', () => add_tag_to_field('receipt_email_body', I18n.t('tags.value')));
-  $("#add_competence_to_body_btn").on('click', () => add_tag_to_field('receipt_email_body', I18n.t('tags.competence')));
-}
+export default ReceiptEmailsForm;
--- app/frontend/javascripts/receipt_emails/modals.js
+++ app/frontend/javascripts/receipt_emails/modals.js
@@ -4,92 +4,104 @@ import I18n from './../i18n'
 import { attr_values } from './../form_commons'
 import ServerFunctions from './../server_functions'
 
-$(() => { if(on_controller('receipt_emails')) receipt_emails_modals() });
+$(() => { if(on_controller('receipt_emails')) new ReceiptEmailsModals() });
 
 export const clean_resend_modal = () => $('#resend_competence').val(default_competence());
 export const clean_send_test_modal = () => $('#send_test_competence').val(default_competence());
 const default_competence = () => `${("0" + (new Date().getMonth()+1)).slice(-2)}/${new Date().getFullYear()}`
 
-const receipt_emails_modals = () => {
-  $('#modal_save_btn').on('click', () => document.getElementById("main_form").submit())
+const ReceiptEmailsModals = (function() {
+  function ReceiptEmailsModals() {
+    this.setup_listeners = () => {
+      const self = this;
 
-  $("#resend_email_modal .btn-ok").on('click', function() {
-    display_info(I18n.t('alert.email.resending'));
-    $('#resend_email_modal').modal('hide');
+      $('#modal_save_btn').on('click', () => document.getElementById("main_form").submit())
 
-    const id = $('#receipt_email_id').val();
-    $.ajax(ReceiptEmailsModals.send_request(
-      ServerFunctions.paths.resend_receipt_email(id),
-      $('#resend_competence').val()
-    ));
-  });
+      $("#resend_email_modal .btn-ok").on('click', function() {
+        display_info(I18n.t('alert.email.resending'));
+        $('#resend_email_modal').modal('hide');
 
-  $("#send_test_email_modal .btn-ok").on('click', function() {
-    display_info(I18n.t('alert.email.sending_test'));
-    $('#send_test_email_modal').modal('hide');
+        const id = $('#receipt_email_id').val();
+        $.ajax(self.send_request(
+          ServerFunctions.paths.resend_receipt_email(id),
+          $('#resend_competence').val()
+        ));
+      });
 
-    const id = $('#receipt_email_id').val();
-    $.ajax(ReceiptEmailsModals.send_request(
-      ServerFunctions.paths.send_test_receipt_email(id),
-      $('#send_test_competence').val()
-    ));
-  });
+      $("#send_test_email_modal .btn-ok").on('click', function() {
+        display_info(I18n.t('alert.email.sending_test'));
+        $('#send_test_email_modal').modal('hide');
 
-  const add_history_item = email => {
-    $('#history_table > tbody:last-child').append(
-      `<tr>
-        <td>${email.date}<\/td>
-        <td>${email.value}<\/td>
-        <td>${email.type}<\/td>
-        <td>${email.user}<\/td>
-      <\/tr>`
-    );
-  }
+        const id = $('#receipt_email_id').val();
+        $.ajax(self.send_request(
+          ServerFunctions.paths.send_test_receipt_email(id),
+          $('#send_test_competence').val()
+        ));
+      });
+    }
 
-  const add_recent_email = email => {
-    $('#recent_emails_table > tbody:last-child').append(
-      `<tr>
-        <td>${email.date}<\/td>
-        <td>${email.maintainer}<\/td>
-        <td>${email.type}<\/td>
-      <\/tr>`
-    );
-  }
+    this.add_history_item = email => {
+      $('#history_table > tbody:last-child').append(
+        `<tr>
+          <td>${email.date}<\/td>
+          <td>${email.value}<\/td>
+          <td>${email.type}<\/td>
+          <td>${email.user}<\/td>
+        <\/tr>`
+      );
+    }
 
-  const send_request = (url, competence) => {
-    return { 
-      url,
-      type: 'POST',
-      beforeSend: xhr => {
-        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
-      },
-      data: {
-        competence,
-        receipt_email: {
-          id: $('#receipt_email_id').val(),
-          recipients_array: $('#receipt_email_recipients_array').val(),
-          value: $('#receipt_email_value').val(),
-          maintainer: $('#receipt_email_id').val(),
-          title: $('#receipt_email_title').val(),
-          body: $('#receipt_email_body').val(),
-          active: $('#model_status').val()
+    this.add_recent_email = email => {
+      $('#recent_emails_table > tbody:last-child').append(
+        `<tr>
+          <td>${email.date}<\/td>
+          <td>${email.maintainer}<\/td>
+          <td>${email.type}<\/td>
+        <\/tr>`
+      );
+    }
+
+    this.send_request = (url, competence) => {
+      return { 
+        url,
+        type: 'POST',
+        beforeSend: xhr => {
+          xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+        },
+        data: {
+          competence,
+          receipt_email: {
+            id: $('#receipt_email_id').val(),
+            recipients_array: $('#receipt_email_recipients_array').val(),
+            value: $('#receipt_email_value').val(),
+            maintainer: $('#receipt_email_id').val(),
+            title: $('#receipt_email_title').val(),
+            body: $('#receipt_email_body').val(),
+            active: $('#model_status').val()
+          }
+        },
+        success: response => {
+          display_notice(response.message);
+          this.add_history_item(response.model);
+        },
+        error: response => {
+          parse_json_errors(response.responseText);
         }
-      },
-      success: response => {
-        display_notice(response.message);
-        ReceiptEmailsModals.add_history_item(response.model);
-      },
-      error: response => {
-        parse_json_errors(response.responseText);
-      }
-    };
+      };
+    }
+
+    this.add_hidden_field = (form_id, key, value) => {
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = `receipt_email[${key}]`;
+      input.value = value;
+      document.getElementById(form_id).appendChild(input);
+    }
+
+    this.setup_listeners();
   }
 
-  const add_hidden_field = (form_id, key, value) => {
-    const input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = `receipt_email[${key}]`;
-    input.value = value;
-    document.getElementById(form_id).appendChild(input);
-  }
-}
+  return ReceiptEmailsModals;
+}());
+
+export default ReceiptEmailsModals;
